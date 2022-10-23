a35d4e09a4f8425022399f99707a0d1c3fceba8f
max gammer <gammermax@gmail.com>
2022-10-23 23:08:23 +0500

    +

--- xAPI_examples/Source.cpp
+++ xAPI_examples/Source.cpp
@@ -2,10 +2,75 @@
 
 #include <curl/curl.h>
 
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+
 
 int main()
 {
 	std::cout << "test" << std::endl;
+
+
+
+
+
+    {
+        //std::ifstream f("example.json");
+       // json data = json::parse(f);
+
+        // create an empty structure (null)
+        json j;
+
+        // add a number that is stored as double (note the implicit conversion of j to an object)
+        j["pi"] = 3.141;
+
+        // add a Boolean that is stored as bool
+        j["happy"] = true;
+
+        // add a string that is stored as std::string
+        j["name"] = "Niels";
+
+        // add another null object by passing nullptr
+        j["nothing"] = nullptr;
+
+        // add an object inside the object
+        j["answer"]["everything"] = 42;
+
+        // add an array that is stored as std::vector (using an initializer list)
+        j["list"] = { 1, 0, 2 };
+
+        // add another object (using an initializer list of pairs)
+        j["object"] = { {"currency", "USD"}, {"value", 42.99} };
+
+        // instead, you could also write (which looks very similar to the JSON above)
+        json j2 = {
+          {"pi", 3.141},
+          {"happy", true},
+          {"name", "Niels"},
+          {"nothing", nullptr},
+          {"answer", {
+            {"everything", 42}
+          }},
+          {"list", {1, 0, 2}},
+          {"object", {
+            {"currency", "USD"},
+            {"value", 42.99}
+          }}
+        };
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     std::string URL = "https://cloud.scorm.com/ScormEngineInterface/TCAPI/public/statements";
     std::string loginpassword = "test:test";
